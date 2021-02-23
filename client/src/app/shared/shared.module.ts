52918e2bf343dd60f439f3d11d05813d3e068461
Yann Provoost <yann.provoost@gmail.com>
2021-02-23 13:22:13 +0100

    added page component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { BodyComponent } from './components/body/body.component';
 import { HeadlineComponent } from './components/headline/headline.component';
 import { TitleComponent } from './components/title/title.component';
 import { SubheadingComponent } from './components/subheading/subheading.component';
+import { PageComponent } from './components/page/page.component';
 
 @NgModule({
-  declarations: [BodyComponent, HeadlineComponent, TitleComponent, SubheadingComponent],
+  declarations: [BodyComponent, HeadlineComponent, TitleComponent, SubheadingComponent, PageComponent],
   imports: [CommonModule],
-  exports: [BodyComponent, HeadlineComponent, TitleComponent, SubheadingComponent],
+  exports: [BodyComponent, HeadlineComponent, TitleComponent, SubheadingComponent, PageComponent],
 })
 export class SharedModule {}
